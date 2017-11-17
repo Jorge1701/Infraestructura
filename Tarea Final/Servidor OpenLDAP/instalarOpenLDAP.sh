a81e3e630cf9d3671d907c10bf3e99a2800255fe
Jorge1701 <kopxejr@gmail.com>
2017-11-17 15:27:04 -0300

    Create instalarOpenLDAP.sh

---
+++ Tarea Final/Servidor OpenLDAP/instalarOpenLDAP.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get build-essential -y
+
+wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.45.tgz
+tar zxvf openldap-2.4.45.tgz 
+cd openldap-2.4.45
+./configure --sysconfdir=/etc
+make
+make install
