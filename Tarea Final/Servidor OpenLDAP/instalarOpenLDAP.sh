675967c36e12e9cf0d360ed65cd44c9b6fd5e584
Jorge1701 <kopxejr@gmail.com>
2017-11-25 20:42:57 -0300

    Update instalarOpenLDAP.sh

--- Tarea Final/Servidor OpenLDAP/instalarOpenLDAP.sh
+++ Tarea Final/Servidor OpenLDAP/instalarOpenLDAP.sh
@@ -7,6 +7,7 @@ apt-get build-dep openldap -y
 wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.45.tgz
 tar zxvf openldap-2.4.45.tgz 
 cd openldap-2.4.45
-./configure --sysconfdir=/etc
+./configure --sysconfdir=/etc --with-cyrus-sasl --with-threads --with-tls --with-yielding-select --with-mp --enable-slapd  --enable-slurpd --enable-cleartext --enable-bdb --enable-ldap --enable-monitor --enable-perl --enable-sql --enable-syslog  --enable-spasswd
+make depend
 make
 make install
