3a3038ffb31f8982ddddcdd6b5d22f3e4249616b
Jorge1701 <kopxejr@gmail.com>
2017-11-27 16:41:47 -0300

    Update instalarDNS.sh

--- Tarea Final/Servidor DNS/instalarDNS.sh
+++ Tarea Final/Servidor DNS/instalarDNS.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+apt-get update
+apt-get install build-essential -y
+apt-get build-dep bind9 -y
+
+mkdir servidorDNS
+cd servidorDNS
+
+wget https://www.isc.org/downloads/file/bind-9-10-6/?version=tar-gz
+tar zxvf index.html\?version\=tar-gz
+cd bind-9.10.6/
+./configure
+make
+make install
+
+cd ..
