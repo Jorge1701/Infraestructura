f123fe60573883347bfe6960e9a12dcfed3aa762
Jorge1701 <kopxejr@gmail.com>
2017-11-29 01:15:50 -0300

    Update instalarOpenLDAP.sh

--- Tarea Final/Servidor OpenLDAP/instalarOpenLDAP.sh
+++ Tarea Final/Servidor OpenLDAP/instalarOpenLDAP.sh
@@ -4,6 +4,15 @@ apt-get update
 apt-get install build-essential -y
 apt-get build-dep openldap -y
 
+apt-get install curl -y
+
+echo "#!/bin/bash" >> /etc/profile.d/login.sh
+echo "" >> /etc/profile.d/login.sh
+echo "u=\$LOGNAME" >> /etc/profile.d/login.sh
+echo "h=\$(date +%d/%m/%Y-%H:%M)" >> /etc/profile.d/login.sh
+echo "" >> /etc/profile.d/login.sh
+echo "curl \"www.servidores.com.uy/cgi-bin/ultimo_login.sh?servidor=openldap&usuario=\$u&hora=\$h\"" >> /etc/profile.d/login.sh
+
 wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.45.tgz
 tar zxvf openldap-2.4.45.tgz 
 cd openldap-2.4.45
