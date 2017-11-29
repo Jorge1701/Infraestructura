91eb99dc1c5ec2d59133509a0235f970954c8d36
Jorge1701 <kopxejr@gmail.com>
2017-11-28 23:52:21 -0300

    Update instalarDNS.sh

--- Tarea Final/Servidor DNS/instalarDNS.sh
+++ Tarea Final/Servidor DNS/instalarDNS.sh
@@ -16,7 +16,7 @@ make install
 
 echo "key \"rndc-key\" {" > /etc/rndc.key
 echo "        algorithm hmac-md5;" >> /etc/rndc.key
-echo "        secret "DTngw5O8I5Axx631GjQ9pA==";" >> /etc/rndc.key
+echo "        secret \"DTngw5O8I5Axx631GjQ9pA==\";" >> /etc/rndc.key
 echo "};" >> /etc/rndc.key
 
 cd ..
