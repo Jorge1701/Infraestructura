0de30b0b8f96f087f32bb7957176112dd86d3cc5
Jorge1701 <kopxejr@gmail.com>
2017-11-28 23:48:04 -0300

    Update configurarDNS.sh

--- Tarea Final/Servidor DNS/configurarDNS.sh
+++ Tarea Final/Servidor DNS/configurarDNS.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+correo=192.168.10.251
+web=192.168.10.253
+openldap192.168.10.149
+nfs=192.168.10.101
+
 echo "key \"rndc-key\" {" > /etc/rndc.key
 echo "        algorithm hmac-md5;" >> /etc/rndc.key
 echo "        secret "DTngw5O8I5Axx631GjQ9pA==";" >> /etc/rndc.key
@@ -33,4 +38,41 @@ echo "        type master;" >> $n
 echo "        file \"/etc/namedb/servidores.com.uy.db\";" >> $n
 echo "};" >> $n
 
-cp *.db /etc/namedb/
+in=/etc/namedb/infraestructura.com.uy.db
+
+echo "\$TTL    604800" > "$in"
+echo "@       IN      SOA     ns.infraestructura.com.uy. root.infraestructura.com.uy. (" >> "$in"
+echo "                              2         ; Serial" >> "$in"
+echo "                         604800         ; Refresh" >> "$in"
+echo "                          86400         ; Retry" >> "$in"
+echo "                        2419200         ; Expire" >> "$in"
+echo "                         604800 )       ; Negative Cache TTL" >> "$in"
+echo ";" >> "$in"
+echo "        NS ns           ; debian server" >> "$in"
+echo "        MX 10 ns        ; mail exchanger" >> "$in"
+echo "" >> "$in"
+echo "ns      A        $web" >> "$in"
+echo "" >> "$in"
+echo "infraestructura.com.uy      IN      CNAME   ns" >> "$in"
+echo "mail    IN      A       $correo" >> "$in"
+echo "www     IN      A       $web" >> "$in"
+echo "ldap    IN      A       $openldap" >> "$in"
+echo "ftp     IN      A       $nfs" >> "$in"
+
+sr=/etc/namedb/servidores.com.uy.db
+
+echo "\$TTL    604800" > "$sr"
+echo "@       IN      SOA     ns.servidores.com.uy. root.servidores.com.uy. (" >> "$sr"
+echo "                              2         ; Serial" >> "$sr"
+echo "                         604800         ; Refresh" >> "$sr"
+echo "                          86400         ; Retry" >> "$sr"
+echo "                        2419200         ; Expire" >> "$sr"
+echo "                         604800 )       ; Negative Cache TTL" >> "$sr"
+echo ";" >> "$sr"
+echo "        NS ns           ; debian server" >> "$sr"
+echo "        MX 10 ns        ; mail exchanger" >> "$sr"
+echo "" >> "$sr"
+echo "ns      A        $web" >> "$sr"
+echo "" >> "$sr"
+echo "servidores.com.uy      IN      CNAME   ns" >> "$sr"
+echo "www     IN      A       $web" >> "$sr"
